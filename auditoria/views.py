58a71b117049ec518bed9e8448eada12ec27d5fa
balemn <enriquev.rodriguez@gmail.com>
2013-08-14 18:01:54 -0400

    Primera presentacion

--- auditoria/views.py
+++ auditoria/views.py
@@ -4,16 +4,31 @@ from auditoria.form import ResultadoForm, LoginForm
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
-from django.forms import ModelForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-import MySQLdb
+import ho.pisa as pisa
+import cStringIO as StringIO
+import cgi
+from django.template.loader import render_to_string
+from django.shortcuts import get_object_or_404
 
 @login_required(login_url='/login')
 def mostrar_encuestas(request):
-	hoja = HojaControl.objects.all()
+	hoja = HojaControl.objects.filter(activa='activo')
 	return render_to_response('mostrar_encuestas.html', {'hoja':hoja}, context_instance=RequestContext(request) )
+
+@login_required(login_url='/login')
+def mostrar_informes(request):
+	hoja = HojaControl.objects.filter(activa= 'Inactivo')
+	return render_to_response('listar_informes.html', {'hoja':hoja}, context_instance=RequestContext(request) )
+
+@login_required(login_url='/login')
+def ver_hoja(request, encuesta_id):
+	hojas = HojaControl.objects.filter(id=encuesta_id, activa='Inactivo')
+	variables = Resultado.objects.filter(hoja_id=encuesta_id)
+	ctx = {'hojas': hojas, 'variables':variables }
+	return render_to_response('hoja_informe.html', ctx, context_instance=RequestContext(request))
 
 @login_required(login_url='/login')
 def add_hoja(request, encuesta_id):
@@ -26,13 +41,14 @@ def add_hoja(request, encuesta_id):
 			formulario.extend(['%s=%s' % (key, val) for val in valuelist])
 			if key != 'csrfmiddlewaretoken' and key != 'enviar-mark' and val != 0 and val and 'null':
 				resultado_update(encuesta_id, key, val)
+			if key == 'enviar-mark' and val == 'on':
+				desactivar_hoja(request, encuesta_id)
 		return render_to_response('datos.html', { 'formulario': formulario}, context_instance=RequestContext(request))
 	else:
 		formulario = ResultadoForm()
 	ctx = {'hojas': hojas, 'variables':variables, 'formulario': formulario}
 	return render_to_response('add_hoja.html', ctx, context_instance=RequestContext(request))
 
-@login_required(login_url='/login')
 def resultado_update(hoja_id, variable_id, valor):
 	resultados = Resultado.objects.get(hoja = hoja_id, variable = variable_id)
 	resultados.valor = valor
@@ -61,3 +77,35 @@ def ingresar(request):
 def salir(request):
 	logout(request)
 	return HttpResponseRedirect('/login')
+
+@login_required(login_url='/login')
+def desactivar_hoja(request, encuesta_id):
+	hoja = HojaControl.objects.get(id=encuesta_id)
+	hoja.activa = 'Inactivo'
+	hoja.save()
+
+def generar_pdf(html):
+    result = StringIO.StringIO()
+    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("UTF-8")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), mimetype='application/pdf')
+    return HttpResponse('Error al generar el PDF: %s' % cgi.escape(html))
+
+@login_required(login_url='/login')
+def hoja_pdf(request, id):
+    hoja=get_object_or_404(HojaControl, id=id)
+    html = render_to_string('hoja_pdf.html', {'pagesize':'A4', 'hoja':hoja}, context_instance=RequestContext(request))
+    return generar_pdf(html)
+
+@login_required(login_url='/login')
+def base(request):
+	ctx = {}
+	return render_to_response('base.html', ctx, context_instance=RequestContext(request))
+
+@login_required(login_url='/login')
+def ver_hoja_pdf(request, encuesta_id):
+	hojas = get_object_or_404(HojaControl, id=encuesta_id)
+	variables = Resultado.objects.filter(hoja_id=encuesta_id)
+	ctx = {'hojas': hojas, 'variables':variables }
+	html = render_to_response('hoja_informe.html', ctx, context_instance=RequestContext(request))
+	return generar_pdf(html)
--- riesgo/urls.py
+++ riesgo/urls.py
@@ -15,8 +15,13 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hoja/$','auditoria.views.mostrar_encuestas'),
+    url(r'^informes/$','auditoria.views.mostrar_informes'),
+    url(r'^informes/(?P<encuesta_id>.*)/$','auditoria.views.ver_hoja'),
+    url(r'^imprimir/informe/(?P<id>.*)$','auditoria.views.ver_hoja_pdf'),
+    url(r'^$','auditoria.views.base'),
     url(r'^hoja/(?P<encuesta_id>.*)/$','auditoria.views.add_hoja'),
     url(r'^login/$','auditoria.views.ingresar', name='vista_login'),
     url(r'^logout/$','auditoria.views.salir', name='vista_logout'),
+    url(r'^pdf_generate/(?P<id>.*)$','auditoria.views.hoja_pdf'),
     #url(r'^encuesta/add/$','auditoria.views.add_encuesta'),
 )
